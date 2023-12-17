207df7c6163493340129b3664ac2c2366c717825
PedroHercules <pedroherculesdantas@gmail.com>
2023-12-17 19:20:28 -0300

    Update compiler configuration to use 'production' environment variable

--- next.config.js
+++ next.config.js
@@ -25,7 +25,7 @@ const nextConfig = {
       return config
    },
    compiler: {
-      removeConsole: process.env.NEXT_PUBLIC_NODE_ENV === 'prod' ? true : false
+      removeConsole: process.env.NEXT_PUBLIC_NODE_ENV === 'production' ? true : false
    },
    images: {
       domains: ['random.imagecdn.app', 'source.unsplash.com', 'descier-tcc.s3.sa-east-1.amazonaws.com']
