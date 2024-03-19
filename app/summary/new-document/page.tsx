e3fe6157e40e018c1bf7fbc4b4deac4638920e35
rafaeelricco <rafaelricco10@gmail.com>
2024-03-18 21:23:32 -0300

    fix: add revalidation to fetch request headers

--- app/summary/new-document/page.tsx
+++ app/summary/new-document/page.tsx
@@ -7,7 +7,8 @@ export default async function SubmitNewPaperPage() {
    const fetchPublicJournals = async () => {
       const request = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/journals/public`, {
          method: 'GET',
-         headers: { 'Content-Type': 'application/json' }
+         headers: { 'Content-Type': 'application/json' },
+         next: { revalidate: 60 }
       })
 
       const response = (await request.json()) as PublicJournalsProps
