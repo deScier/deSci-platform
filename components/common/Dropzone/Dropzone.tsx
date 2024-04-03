95e42838bb9ca2e34b1e61b791c1d06d4386553e
rafaeelricco <rafaelricco10@gmail.com>
2024-04-03 20:55:09 -0300

    fix: update Dropzone component

--- components/common/Dropzone/Dropzone.tsx
+++ components/common/Dropzone/Dropzone.tsx
@@ -1,9 +1,8 @@
 'use client'
 import { formatFileName } from '@/utils/format_file_name'
 import * as Button from '@components/common/Button/Button'
-import Image from 'next/image'
 import React, { useEffect } from 'react'
-import { FileEarmarkText, Upload } from 'react-bootstrap-icons'
+import { Eye, FileEarmarkText, Upload } from 'react-bootstrap-icons'
 import { Accept, useDropzone } from 'react-dropzone'
 import { twMerge } from 'tailwind-merge'
 import { DropzoneProps, StoredFile } from './Typing'
@@ -100,8 +99,8 @@ const Dropzone = React.forwardRef(({ setSelectedFile, setValue, placeholder, mes
          <div>
             <div
                className={twMerge(
-                  'grid border-[1px] border-dashed border-blue-light rounded-lg p-4 transition duration-300 ease-in-out items-center bg-[#F1FFFF] py-6 min-h-[140px]',
-                  `${thumbnail && 'h-44 w-full p-0'}`
+                  'grid border-[1px] border-dashed border-blue-light rounded-lg p-4 transition duration-300 ease-in-out items-center bg-[#F1FFFF] py-6 min-h-[140px]'
+                  //   `${thumbnail && 'h-44 w-full p-0'}`
                )}
             >
                <div {...getRootProps({ className: 'dropzone flex flex-col gap-2' })} className="overflow-hidden relative">
@@ -124,7 +123,7 @@ const Dropzone = React.forwardRef(({ setSelectedFile, setValue, placeholder, mes
                   ) : (
                      <React.Fragment>
                         <div className="flex flex-col items-center justify-center gap-2">
-                           {thumbnail ? (
+                           {/* {thumbnail ? (
                               <Image
                                  width={1920}
                                  height={1080}
@@ -132,26 +131,39 @@ const Dropzone = React.forwardRef(({ setSelectedFile, setValue, placeholder, mes
                                  alt="Thumbnail"
                                  className="w-full h-44 object-cover rounded-md brightness-50"
                               />
-                           ) : null}
-                           <div className={twMerge('grid gap-2 items-center content-center absolute', `${thumbnail === false && 'relative'}`)}>
+                           ) : null} */}
+                           <div className={twMerge('grid gap-2 items-center content-center relative', `${thumbnail === false && 'relative'}`)}>
                               <div className="grid justify-item-center gap-2 grid-flow-col justify-center items-center">
                                  <FileEarmarkText
-                                    className={twMerge('w-4 h-4 sm:w-5 sm:h-5 sm:mb-[2px] fill-white', `${thumbnail === false && 'fill-black'}`)}
+                                    className={twMerge(
+                                       'w-4 h-4 sm:w-5 sm:h-5 sm:mb-[2px] fill-black'
+                                       /* `${thumbnail === false && 'fill-black'}` */
+                                    )}
                                  />
                                  <p
                                     className={twMerge(
-                                       'text-xs sm:text-sm font-semibold text-white text-center',
-                                       `${thumbnail === false && 'text-black'}`
+                                       'text-xs sm:text-sm font-semibold text-black text-center'
+                                       //    `${thumbnail === false && 'text-black'}`
                                     )}
                                  >
                                     {formatFileName(files[0]?.name)}
                                  </p>
+                                 <p className="text-sm font-regular text-center">
+                                    {files[0]?.size ? `${(files[0]?.size / 1024).toFixed(2)} KB` : '0 KB'}
+                                 </p>
+                                 <Eye
+                                    className={twMerge(
+                                       'w-4 h-4 sm:w-5 sm:h-5 sm:mb-[2px] fill-black hover:fill-primary-main cursor-pointer z-50'
+                                       /* `${thumbnail === false && 'fill-black'}` */
+                                    )}
+                                    onClick={() => window.open(files[0]?.preview, '_blank')}
+                                 />
                               </div>
                               <Button.Button
                                  variant="outline"
                                  className={twMerge(
-                                    'py-2 px-6 text-sm w-fit mx-auto my-0 border-white text-white hover:bg-transparent',
-                                    `${thumbnail === false && 'border-primary-main text-primary-main hover:bg-transparent'}`
+                                    'py-2 px-6 text-sm w-fit mx-auto my-0 border-primary-main text-primary-main hover:bg-transparent'
+                                    // `${thumbnail === false && 'border-primary-main text-primary-main hover:bg-transparent'}`
                                  )}
                                  onClick={() => {
                                     setFiles([])
