669ff6a4d91091935cd4ad2f02b6c2d573911fa9
rafaeelricco <rafaelricco10@gmail.com>
2024-10-06 11:00:54 -0300

    feat: refactor fetchSingleArticle function and remove commented code

--- app/descier/articles-for-approval/[id]/page.tsx
+++ app/descier/articles-for-approval/[id]/page.tsx
@@ -116,68 +116,6 @@ export default function ArticleForApprovalPage({ params }: { params: { id: strin
 
    const { append, remove, fields: keywords } = useFieldArray({ name: 'keywords', control: control })
 
-   //    const fetchSingleArticle = async (documentId: string) => {
-   //       await fetch_article(documentId).then((res) => {
-   //          setArticle(res as DocumentGetProps)
-   //          setNftData({
-   //             nftHash: res?.document.nftHash || '',
-   //             nftLink: res?.document.nftLink || '',
-   //             nftAmount: res?.document.nftAmount || 1
-   //          })
-   //          const access = res?.document.accessType === 'FREE' ? 'open-access' : 'paid-access'
-   //          setAccessType(access)
-   //          getApprovals(res?.document.reviewersOnDocuments || [])
-
-   //          setValue('title', res?.document.title || '')
-   //          setValue('abstract', res?.document.abstract || '')
-   //          setValue('field', res?.document.field || '')
-   //          setValue('documentType', res?.document.documentType || '')
-   //          setDocumentType(res?.document.documentType || '')
-   //          setValue('accessType', (res?.document.accessType || 'FREE') as 'PAID' | 'FREE')
-   //          setValue('price', res?.document.price?.toString() || '')
-   //          setValue('journalId', res?.document.id || '')
-   //          setValue('category', res?.document.category || '')
-   //          setValue('cover', {
-   //             lastModified: 0,
-   //             lastModifiedDate: new Date(),
-   //             name: res?.document.cover || '',
-   //             path: res?.document.cover || '',
-   //             preview: res?.document.cover || '',
-   //             size: 0,
-   //             type: ''
-   //          })
-   //          setValue('file', {
-   //             lastModified: 0,
-   //             lastModifiedDate: new Date(),
-   //             name: res?.document.documentVersions?.[0]?.fileName || '',
-   //             path: '',
-   //             preview: '',
-   //             size: 0,
-   //             type: ''
-   //          })
-
-   //          const keywordsArray =
-   //             res?.document.keywords?.split(';').map((keyword) => ({
-   //                id: uniqueId('key'),
-   //                name: keyword.trim()
-   //             })) || []
-   //          setValue('keywords', keywordsArray)
-
-   //          const authorsArray =
-   //             res?.document.authorsOnDocuments?.map((author) => ({
-   //                id: author.id,
-   //                name: author.author?.name || '',
-   //                email: author.author?.email || '',
-   //                title: author.author?.title || '',
-   //                revenuePercent: author.revenuePercent?.toString() || '0',
-   //                walletAddress: author.author?.walletAddress || ''
-   //             })) || []
-   //          setValue('authors', authorsArray)
-
-   //          trigger()
-   //       })
-   //    }
-
    const fetchSingleArticle = async (documentId: string) => {
       try {
          const res = await fetch_article(documentId)
